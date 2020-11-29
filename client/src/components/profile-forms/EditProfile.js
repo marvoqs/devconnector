16f84fe99950323a3c5b6fde5d1a30e03aa31422
marvoqs <marvoqs@gmail.com>
2020-11-29 21:44:26 +0100

    load user socials correctly

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -32,13 +32,13 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
       skills: loading || !profile.skills ? '' : profile.skills,
       githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
       bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.twitter ? '' : profile.twitter,
-      facebook: loading || !profile.facebook ? '' : profile.facebook,
-      linkedin: loading || !profile.linkedin ? '' : profile.linkedin,
-      youtube: loading || !profile.youtube ? '' : profile.youtube,
-      instagram: loading || !profile.instagram ? '' : profile.instagram,
+      twitter: loading || !profile.social.twitter ? '' : profile.social.twitter,
+      facebook: loading || !profile.social.facebook ? '' : profile.social.facebook,
+      linkedin: loading || !profile.social.linkedin ? '' : profile.social.linkedin,
+      youtube: loading || !profile.social.youtube ? '' : profile.social.youtube,
+      instagram: loading || !profile.social.instagram ? '' : profile.social.instagram,
     });
-  }, [loading]);
+  }, [loading, getCurrentProfile]);
 
   const { company, website, location, status, skills, githubusername, bio, twitter, facebook, linkedin, youtube, instagram } = formData;
 
