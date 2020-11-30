50800894c56e48c310c2a4a0a3861475aac4d9be
marvoqs <marvoqs@gmail.com>
2020-11-30 17:38:02 +0100

    remove parentheses from the Date.now

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: "user",
+    ref: 'user',
   },
   text: {
     type: String,
@@ -20,7 +20,7 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: "user",
+        ref: 'user',
       },
     },
   ],
@@ -28,7 +28,7 @@ const PostSchema = new Schema({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: "user",
+        ref: 'user',
       },
       text: {
         type: String,
@@ -42,14 +42,14 @@ const PostSchema = new Schema({
       },
       date: {
         type: Date,
-        default: Date.now(),
+        default: Date.now,
       },
     },
   ],
   date: {
     type: Date,
-    default: Date.now(),
+    default: Date.now,
   },
 });
 
-module.exports = Post = mongoose.model("post", PostSchema);
+module.exports = Post = mongoose.model('post', PostSchema);
